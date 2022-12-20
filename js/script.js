882abcd4f7aa22f56415d34c2609a65d4e41d272
Oluwafemi Micheal <119140798+micheal-Oluwafemi@users.noreply.github.com>
2022-12-20 22:15:29 +0100

    Added Initial files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+// Function
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  nav.classList.toggle('hidden');
+  nav.classList.toggle('flex');
+});
